da2f4d7807089ca704c7df3809c64a3038d6b1e1
Jean Nguyen <jeannguyen.ca@gmail.com>
2020-02-23 20:32:34 -0800

    Fix link in module 2

---
+++ list.js
@@ -0,0 +1,56 @@
+[
+  {
+      title: "Spaceship (G4 to 6)",
+      module: "module1", 
+      img: "media/firstlesson.png",
+      description: "For Beginning learners: Welcome to VR coding! In this lesson, you will animate 3D object in VR world using a coding concept called <b>variables.</b>"
+    }, 
+    {
+      title: "Traffic System (G4 and up)",
+      module: "module2", 
+      img: "media/roads.png",
+      description: "For Beginning learners: You will learn 3D coding concepts such as <b>3D coordinates</b> and <b>variables</b> while making your own 3D traffic system."
+    },
+    {
+      title: "Waterway (G4 and up) ",
+      module: "module3", 
+      img: "media/boat.png",
+      description: "For Beginning learners: Let's make a boat get to the destination in a 3D world. In this lesson, you will learn basic coding concepts such as <b>variables</b>."
+    },
+    {
+      title: "IF Statements",
+      module: "module4", 
+      img: "media/C3D_app.png",
+      description: "Learn how to use IF statement"
+    },
+    {
+      title: "Loop",
+      module: "module5", 
+      img: "media/C3D_app.png",
+      description: "Learn how to use LOOP"
+    },
+    {
+      title: "Nested Loop",
+      module: "module6", 
+      img: "media/C3D_app.png",
+      description: "Learn how to use NESTED LOOP"
+    },
+    {
+      title: "Functions",
+      module: "module7", 
+      img: "media/C3D_app.png",
+      description: "Learn how to create FUNCTIONS"
+    },
+    {
+      title: "Camera Position",
+      module: "module8", 
+      img: "media/C3D_app.png",
+      description: "Learn how to use CAMERA"
+    },
+    {
+      title: "Coding Challenge",
+      module: "module9", 
+      img: "media/C3D_app.png",
+      description: "You'll face a coding challenge"
+    }
+  ]
